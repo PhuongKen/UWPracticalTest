1d79426a4a32093806fee4f284a464cfba79e7c6
Your Name <you@example.com>
2018-11-01 10:15:54 +0700

    Practical Test UWP

---
+++ UWPPracticalTest/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPPracticalTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            collection = new ObservableCollection<RootObject>();
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<RootObject> collection { get; set; }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            List<RootObject> data = await POJO.GetData();
+            for (int i = 0; i < data.Count; i++)
+            {
+                collection.Add(data[i]);
+            }
+            APIGridView.ItemsSource = collection;
+        }
+    }
+}
---
+++ UWPPracticalTest/POJO.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPPracticalTest
+{
+    public class POJO
+    {
+        public async static Task<List<RootObject>> GetData()
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://youtube-video-api-1608.appspot.com/youtube/api");
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<RootObject> data = JsonConvert.DeserializeObject<List<RootObject>>(result);
+
+            return data;
+        }
+    }
+
+    public class RootObject
+    {
+        public string videoId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string keywords { get; set; }
+        public string category { get; set; }
+        public string genre { get; set; }
+        public string authorName { get; set; }
+        public string authorEmail { get; set; }
+        public string birthday { get; set; }
+    }
+}
